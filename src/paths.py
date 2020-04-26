fd79fb75018f102e4f803fc45a77bb667ab6e9bb
Patrickdg <deguzman.patrick@outlook.com>
2020-04-26 13:04:06 -0400

    general data review, additional paths

--- src/paths.py
+++ src/paths.py
@@ -7,8 +7,12 @@ bus_path_processed = 'data/processed/ttc/bus/bus_data.csv'
 
 bus_path_cleaned = 'data/cleaned_final/ttc/bus/bus_data.csv'
 
+## readmes
+sc_readme = 'data/raw/ttc/docs/ttc-streetcar-delay-data-readme.xlsx'
 
 # TTC RIDERSHIP
 ridership_path_processed = 'data/processed/ridership/ridership.csv'
 
 # WEATHER
+hourly_weather_path = 'data/raw/weather/weatherstats_toronto_hourly.csv'
+daily_weather_path = 'data/raw/weather/weatherstats_toronto_daily.csv'
